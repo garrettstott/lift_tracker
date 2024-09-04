258d9b4519971769ed6cac0eda04d312ddd9a6a6
garrettstott <garrettstott@gmail.com>
2024-09-03 18:24:00 -0600

    set timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_time_zone, if: :user_signed_in?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   before_action :authenticate_user!
 
   # before_action -> { flash[:notice] = 'notice'; flash[:error] = 'error'; flash[:success] = 'success'}
+  private
+
+  def set_time_zone
+    Time.zone = 'MST'
+  end
 end
